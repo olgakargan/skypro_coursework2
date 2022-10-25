75c110e863b1d2a9e591354a588277b0e90dd421
Olga Kargan <olgakargan72@gmail.com>
2022-10-25 19:56:59 +0400

    Initial commit

---
+++ src/test/java/com/example/skypro_coursework2/SkyproCoursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skypro_coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCoursework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
